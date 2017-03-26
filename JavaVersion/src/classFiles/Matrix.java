0f405bb10416049864201eb83bbc601a21a05bd9
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-26 11:09:44 -0500

    Implemented determinant function for size equal to 1 or 2

--- JavaVersion/src/classFiles/Matrix.java
+++ JavaVersion/src/classFiles/Matrix.java
@@ -1,12 +1,13 @@
 package classFiles;
+import java.lang.Math;
 
-
+// A matrix with equal rows and columns
 public class Matrix {
 	
 	private int MaxSize = 10;
 	
-	private int n;
-	private double[][] mdata = new double[MaxSize][MaxSize];
+	private int n; // size of matrix
+	private double[][] mdata = new double[MaxSize][MaxSize]; // matrix representation
 	
 	
 	// constructor
@@ -14,9 +15,23 @@ public class Matrix {
 		n = _n;
 	}
 	
+	// returns the determinant of the matrix
 	public double Determinant() {
+		double det = 0.0;
 		
-		return 5;
+		// condition: size of matrix
+		if(n == 1) {
+			det = mdata[0][0];
+		}
+		else if(n == 2) {
+			det = mdata[0][0] * mdata[1][1] - mdata[0][1] * mdata[1][0];
+		}
+		else {
+			for(int i = 0; i < n; i++) {
+				det += pow(-1.0, (double)i) * mdata[0][i] * subMatrix(0, i).determinant();
+			}
+		}
+		return det;
 	}
 	
 	public Matrix inverse() {
