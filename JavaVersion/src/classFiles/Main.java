ce0589afdc40a5513719dd4447f17f0f5de2c204
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-26 11:40:20 -0500

    Added try-catch for initializing file stream

--- JavaVersion/src/classFiles/Main.java
+++ JavaVersion/src/classFiles/Main.java
@@ -1,5 +1,5 @@
 package classFiles;
-
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
@@ -8,5 +8,15 @@ public class Main {
 		{
 			System.out.println("Error - Expected usage: ./main input.txt output.txt");
 		}
+		
+		// initialize file stream
+		try {
+			FileInputStream in = new FileInputStream(args[1]);
+			FileOutputStream out = new FileOutputStream(args[2]);
+		}
+		catch (Exception e) {
+			System.out.println("Error - File Not found");
+		}
+		
 	}
 }
