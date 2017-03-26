fed558f3dd4025c956375769f2cb8e95fab714dc
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-26 11:34:42 -0500

    Implemented check for number of arguments in main

---
+++ JavaVersion/src/classFiles/Main.java
@@ -0,0 +1,12 @@
+package classFiles;
+
+
+public class Main {
+	public static void main(String[] args) {
+		// Confirm command-line parameters
+		if (args.length < 3)
+		{
+			System.out.println("Error - Expected usage: ./main input.txt output.txt");
+		}
+	}
+}
