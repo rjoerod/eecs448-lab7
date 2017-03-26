6113ed60bcb84ce77608c4a34a64be9431aee732
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-26 10:59:48 -0500

    Setup layout of Matrix class

---
+++ JavaVersion/src/classFiles/Matrix.java
@@ -0,0 +1,31 @@
+package classFiles;
+
+
+public class Matrix {
+	
+	private int MaxSize = 10;
+	
+	private int n;
+	private double[][] mdata = new double[MaxSize][MaxSize];
+	
+	
+	// constructo
+	public Matrix() {
+
+	}
+	
+	public double Determinant() {
+		
+		return 5;
+	}
+	
+	public Matrix inverse() {
+		
+		return null;
+	}
+	
+	public Matrix subMatrix(int i, int j) {
+		return null;
+		
+	}
+}
