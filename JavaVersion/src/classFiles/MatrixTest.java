4da88670d5b915313d04c1ae6629b62c9c42ea73
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-27 11:37:43 -0500

    Added JUnit stubs for non-void methods of class Matrix

---
+++ JavaVersion/src/classFiles/MatrixTest.java
@@ -0,0 +1,29 @@
+package classFiles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatrixTest {
+
+	@Test
+	public void testDeterminant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInverse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnElement() {
+		fail("Not yet implemented");
+	}
+
+}
