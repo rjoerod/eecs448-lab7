376a9a7a44e126f7094b6b2b218af7842f014aa6
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-27 11:52:08 -0500

    Added JUnit test method for method determinant in class Matrix

--- JavaVersion/src/classFiles/MatrixTest.java
+++ JavaVersion/src/classFiles/MatrixTest.java
@@ -8,7 +8,26 @@ public class MatrixTest {
 
 	@Test
 	public void testDeterminant() {
-		fail("Not yet implemented");
+		
+		// initialize new matrix
+		Matrix mTest = new Matrix(3);
+		
+		// add elements
+		mTest.addElement(3,0,0);
+		mTest.addElement(2,0,1);
+		mTest.addElement(0,0,2);
+		
+		mTest.addElement(5,1,0);
+		mTest.addElement(2,1,1);
+		mTest.addElement(4,1,2);
+		
+		mTest.addElement(9,2,0);
+		mTest.addElement(0,2,1);
+		mTest.addElement(0,2,2);
+		
+		// test if determinant is 72.0
+		assertEquals( 72.0, mTest.determinant(), 0.001 );
+		
 	}
 
 	@Test
