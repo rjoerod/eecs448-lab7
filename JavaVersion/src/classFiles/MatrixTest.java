840f58beb61cd89145c4d2e01bdb75586c460d04
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-27 11:56:59 -0500

    Adjusted elements in test matrix for JUnit method testDeterminant

--- JavaVersion/src/classFiles/MatrixTest.java
+++ JavaVersion/src/classFiles/MatrixTest.java
@@ -13,26 +13,27 @@ public class MatrixTest {
 		Matrix mTest = new Matrix(3);
 		
 		// add elements
-		mTest.addElement(3,0,0);
+		mTest.addElement(2,0,0);
 		mTest.addElement(2,0,1);
 		mTest.addElement(0,0,2);
 		
 		mTest.addElement(5,1,0);
-		mTest.addElement(2,1,1);
+		mTest.addElement(1,1,1);
 		mTest.addElement(4,1,2);
 		
-		mTest.addElement(9,2,0);
+		mTest.addElement(8,2,0);
 		mTest.addElement(0,2,1);
 		mTest.addElement(0,2,2);
 		
-		// test if determinant is 72.0
-		assertEquals( 72.0, mTest.determinant(), 0.001 );
+		// test if determinant is 64.0
+		assertEquals( 64.0, mTest.determinant(), 0.001 );
 		
 	}
 
 	@Test
 	public void testInverse() {
-		fail("Not yet implemented");
+		
+
 	}
 
 	@Test
