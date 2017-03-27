6a9df97a567b3b03047bb5dcff447ba5e2cf0a97
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-27 12:24:53 -0500

    Added JUnit test for method subMatrix in class Matrix

--- JavaVersion/src/classFiles/MatrixTest.java
+++ JavaVersion/src/classFiles/MatrixTest.java
@@ -75,7 +75,38 @@ public class MatrixTest {
 
 	@Test
 	public void testSubMatrix() {
-		fail("Not yet implemented");
+		// initialize new matrix
+		Matrix mTest = new Matrix(3);
+		
+		// add elements
+		mTest.addElement(2,0,0);
+		mTest.addElement(2,0,1);
+		mTest.addElement(0,0,2);
+		
+		mTest.addElement(5,1,0);
+		mTest.addElement(1,1,1);
+		mTest.addElement(4,1,2);
+		
+		mTest.addElement(8,2,0);
+		mTest.addElement(0,2,1);
+		mTest.addElement(0,2,2);
+		
+		// initialize submatrix
+		Matrix mTestSub = new Matrix(2);
+		
+		// add elements
+		mTestSub.addElement(2,0,0);
+		mTestSub.addElement(2,0,1);
+		
+		mTestSub.addElement(8,1,0);
+		mTestSub.addElement(0,1,1);
+		
+		// test if inverse of mTest is mTestSub
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				assertEquals( mTestSub.returnElement(i,j), (mTest.subMatrix(1,2)).returnElement(i,j), 0.001 );
+			}
+		}	
 	}
 
 	@Test
